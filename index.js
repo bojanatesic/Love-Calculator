6a1c9b64ed62ad1297600c5d967f12ca1bf15fdb
bojanatesic <tesic.bojana@yahoo.com>
2021-10-31 13:15:00 +0100

    if updated

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function perfectMatch(e, percent) {
         input2.value = ''
         return
     }   
-    var percent = Math.round(Math.random()*100);
+    var percent = Math.round((Math.random()+1)*100);
     var outputPercent = document.querySelector('.outputPercent').innerHTML = percent + "%";
-    if (percent < 0.4) {
+    if (percent < 40) {
         var output = document.querySelector('.output').innerHTML = "The chance of a relationship working out between  " + input1.value + " and " + input2.value + " is not very big, but relatioship is very possible, if the two of you really want it to, and are prepared to make some sacrafices fot it.";
       } else {
         var output = document.querySelector('.output').innerHTML = "Oh, wow " + input1.value + " and " + input2.value + " looks like you are each other soulmates.";
